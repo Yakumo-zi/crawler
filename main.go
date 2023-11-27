b85d5b33710dff74296e4dd4a900f79a2abbba6e
Yakumo-zi <1477717283@qq.com>
2023-11-27 19:14:14 +0800

    set proxy

--- main.go
+++ main.go
@@ -2,49 +2,27 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"net/http"
-	"os"
+	"github.com/Yakumo-zi/crawler/collect"
+	"github.com/Yakumo-zi/crawler/proxy"
+	"time"
 )
 
 func main() {
-	url := "https://www.thepaper.cn/"
-	resp, err := http.Get(url)
+	proxyURLs := []string{"http://127.0.0.1:7890"}
+	p, err := proxy.RoundRobinProxySwitcher(proxyURLs...)
 	if err != nil {
-		fmt.Printf("fetch url error:%v\n", err)
-		return
+		fmt.Println("RoundRobinProxySwitcher failed")
+	}
+	url := "https://google.com"
+	var f collect.Fetcher = collect.BrowserFetch{
+		Timeout: 3000 * time.Millisecond,
+		Proxy:   p,
 	}
 
-	defer func(Body io.ReadCloser) {
-		_ = Body.Close()
-	}(resp.Body)
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Error status code:%v\n", resp.StatusCode)
-		return
-	}
-	content, err := io.ReadAll(resp.Body)
+	body, err := f.Get(url)
 	if err != nil {
-		fmt.Printf("read content failed:%v\n", err)
+		fmt.Printf("read content failed:%v\\n", err)
 		return
 	}
-
-	file, err := os.Create("index.html")
-
-	if err != nil {
-		fmt.Printf("create file failed:%v\n", err)
-		return
-	}
-
-	defer func(file *os.File) {
-		_ = file.Close()
-	}(file)
-
-	n, err := file.WriteString(string(content))
-	if err != nil {
-		fmt.Printf("write content to file failed:%v\n", err)
-		return
-	}
-
-	fmt.Printf("write %d bytes to file %s \n", n, file.Name())
+	fmt.Println(string(body))
 }
