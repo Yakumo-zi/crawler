1013562137dd3050010af4aa27c9c2e5f2f3dbf6
Yakumo-zi <1477717283@qq.com>
2024-08-15 21:49:40 +0800

    feat:add fetch and encoding function

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module crawler
 
 go 1.22.6
+
+require (
+	golang.org/x/net v0.28.0
+	golang.org/x/text v0.17.0
+)
--- main.go
+++ main.go
@@ -1,30 +1,45 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"net/http"
+
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 )
 
 func main() {
-	url := "https://thepaper.cn/"
+
+}
+
+func Fetch(url string) ([]byte, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Printf("fetch url error:%v", err)
-		return
+		panic(err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
 		fmt.Printf("Error status code:%v", resp.StatusCode)
-		return
 	}
 
-	body, err := io.ReadAll(resp.Body)
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DeterminEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewEncoder())
+	return io.ReadAll(utf8Reader)
+
+}
+
+func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
 	if err != nil {
-		fmt.Printf("read content failed:%v", err)
-		return
+		fmt.Printf("fetch error:%v", err)
+		return unicode.UTF8
 	}
-
-	fmt.Println("body:", string(body))
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }
